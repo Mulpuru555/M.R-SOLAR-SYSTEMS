f729cbba4a27d8d6314a03eedb888bec5d65c17e
Mulpuru jashwanth <84584485+Mulpuru555@users.noreply.github.com>
2026-02-24 22:27:39 +0530

    Update employee-payments.js

--- employee-payments.js
+++ employee-payments.js
@@ -4,7 +4,7 @@ import { getFirestore, collection, addDoc, serverTimestamp }
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNtPFzFkYLpbv8vgfeQ0_uE42JT7h28bc",
+ apiKey: "AIzaSyBNtPFzFkYLpbv8vgfeQ0_uE42JT7h28bc",
   authDomain: "mr-solar-portal.firebaseapp.com",
   projectId: "mr-solar-portal",
   storageBucket: "mr-solar-portal.firebasestorage.app",
@@ -16,36 +16,45 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const form = document.getElementById("paymentForm");
-const message = document.getElementById("message");
+document.addEventListener("DOMContentLoaded", function () {
 
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "login.html";
+  const form = document.getElementById("paymentForm");
+  const message = document.getElementById("message");
+
+  if (!form) {
+    console.error("Form not found in HTML");
+    return;
   }
-});
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "login.html";
+    }
+  });
 
-  const customerName = document.getElementById("customerName").value;
-  const executiveName = document.getElementById("executiveName").value;
-  const sanctionedAmount = document.getElementById("sanctionedAmount").value;
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-  try {
-    await addDoc(collection(db, "customerPayments"), {
-      customerName,
-      executiveName,
-      sanctionedAmount: Number(sanctionedAmount),
-      paidAmount: 0,
-      createdBy: auth.currentUser.uid,
-      createdAt: serverTimestamp()
-    });
+    const customerName = document.getElementById("customerName").value;
+    const executiveName = document.getElementById("executiveName").value;
+    const sanctionedAmount = document.getElementById("sanctionedAmount").value;
 
-    message.innerText = "Customer Added Successfully!";
-    form.reset();
+    try {
+      await addDoc(collection(db, "customerPayments"), {
+        customerName,
+        executiveName,
+        sanctionedAmount: Number(sanctionedAmount),
+        paidAmount: 0,
+        createdBy: auth.currentUser.uid,
+        createdAt: serverTimestamp()
+      });
 
-  } catch (error) {
-    message.innerText = error.message;
-  }
+      message.innerText = "Customer Added Successfully!";
+      form.reset();
+
+    } catch (error) {
+      message.innerText = error.message;
+    }
+  });
+
 });
